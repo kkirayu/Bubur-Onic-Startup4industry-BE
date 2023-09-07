f201c725e5aa45ee37667ec3656b2f5df9c244cd
moch akbar maulana <moc.akbar119@gmail.com>
2023-09-07 16:25:50 +0700

    add pretty routes

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        PrettyRoutes\ServiceProvider::class,
     ])->toArray(),
 
     /*
