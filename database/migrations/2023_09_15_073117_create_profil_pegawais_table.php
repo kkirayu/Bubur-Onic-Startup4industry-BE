7cc4a205b2009a432d4e1a9bd9e7492e0d9254b7
zackyZAR <zackyaulya20@gmail.com>
2023-09-15 15:56:04 +0700

    #147 adanya api untuk memange profile_pegawai

--- database/migrations/2023_09_15_073117_create_profil_pegawais_table.php
+++ database/migrations/2023_09_15_073117_create_profil_pegawais_table.php
@@ -13,31 +13,31 @@ return new class extends Migration
     {
         Schema::create('profil_pegawais', function (Blueprint $table) {
             $table->id();
-            $table->integer('perusahaan_id');
-            $table->integer('cabang_id');
-            $table->integer('user_id');
+            $table->integer('perusahaan_id')->default(0);
+            $table->integer('cabang_id')->default(0);
+            $table->integer('user_id')->default(0);
             $table->string('kode_pegawai')->unique();
             $table->text('alamat');
-            $table->text('alamat_asal');
+            $table->text('alamat_asal')->nullable();
             $table->string('jenis_kelamin');
             $table->string('agama');
             $table->date('tanggal_lahir');
             $table->date('tanggal_masuk');
-            $table->date('tanggal_keluar');
+            $table->date('tanggal_keluar')->nullable();
             $table->boolean('tanggal_kawin');
             $table->string('nomor_ktp')->nullable();
             $table->string('npwp')->nullable();
 
-            $table->double('gaji_pokok');
-            $table->double('uang_hadir');
-            $table->double('tunjangan_jabatan');
-            $table->double('tunjangan_tambahan');
-            $table->double('extra_rajin');
-            $table->double('thr');
-            $table->double('tunjangan_lembur');
+            $table->double('gaji_pokok')->default(0.0);
+            $table->double('uang_hadir')->default(0.0);
+            $table->double('tunjangan_jabatan')->default(0.0);
+            $table->double('tunjangan_tambahan')->default(0.0);
+            $table->double('extra_rajin')->default(0.0);
+            $table->double('thr')->default(0.0);
+            $table->double('tunjangan_lembur')->default(0.0);
 
-            $table->integer('quota_cuti_tahunan');
-            $table->integer('team_id');
+            $table->integer('quota_cuti_tahunan')->default(0);
+            $table->integer('team_id')->default(0);
 
             $table->timestamps();
             $table->userstamps();
