50bc172f56a5468ce0e31031b41f3c7f8492986f
zackyZAR <zackyaulya20@gmail.com>
2023-09-15 15:58:25 +0700

    #147 adanya api untuk memange profile_pegawai

--- database/migrations/2023_09_15_073117_create_profil_pegawais_table.php
+++ database/migrations/2023_09_15_073117_create_profil_pegawais_table.php
@@ -49,6 +49,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cuti');
+        Schema::dropIfExists('profil_pegawais');
     }
 };
