2bf1d40782913a469618e0ac010c32b55306f8b5
Muhammad Danil <mdanil180215@gmail.com>
2023-09-22 10:59:59 +0700

    rename profil to profile

--- app/Models/ProfilPegawai.php
+++ app/Models/ProfilPegawai.php
@@ -6,7 +6,7 @@ use Laravolt\Crud\CrudModel;
 
 class ProfilPegawai extends CrudModel
 {
-    protected $table = 'profil_pegawais';
+    protected $table = 'profile_pegawais';
 
     protected string $path = "/api/pegawai/profil-pegawai";
 
