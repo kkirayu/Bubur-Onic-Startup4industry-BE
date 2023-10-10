0d5d6c4b9e6443fe1cf7580bc4015aac3b3ac630
Purwa <purwadarozatunakis@gmail.com>
2023-10-10 10:43:24 +0700

    add team view to table dan detail

--- app/Models/ProfilPegawai.php
+++ app/Models/ProfilPegawai.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 use App\Traits\HideCompanyTrait;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Laravolt\Crud\CrudModel;
+use Laravolt\Crud\Spec\BaseTableValue;
 
 class ProfilPegawai extends CrudModel
 {
@@ -12,5 +14,22 @@ class ProfilPegawai extends CrudModel
     protected $table = 'profile_pegawais';
 
     protected string $path = "/api/pegawai/profil-pegawai";
+
+    public function user () : BelongsTo{
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function team () : BelongsTo {
+        return $this->belongsTo(Team::class, 'team_id');
+    }
+    
+
+    public function tableValueMapping(): array
+    {
+        return [
+            new BaseTableValue("user_id", "hasOne", "user", "name"),
+            new BaseTableValue("team_id", "hasOne", "team", "nama"),
+        ];
+    }
 
 }
