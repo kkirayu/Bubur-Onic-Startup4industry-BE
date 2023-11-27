531a459104116fb0b1fb9819bf81196742a3e2d4
Purwa <purwadarozatunakis@gmail.com>
2023-11-27 09:01:41 +0700

    change api  to update status

--- tests/Feature/PotonganKaryawanTest.php
+++ tests/Feature/PotonganKaryawanTest.php
@@ -102,7 +102,7 @@ class PotonganKaryawanTest extends TestCase
         $response->assertStatus(422);
     }
 
-    public function testUpdatePotonganKaryawanDIAJUKAN()
+    public function testUpdatePotonganKaryawan()
     {
         $user = UserFactory::new()->create();
         $team = Team::create([
@@ -218,21 +218,73 @@ class PotonganKaryawanTest extends TestCase
             'cabang_id' => '1'
         ]);
 
-        $response = $this->postJson("api/potongan-karyawan/potongan-karyawan/{$potongan->id}", [
-            'bulan' => 11,
-            'tahun' => 2023,
-            'id_profile_pegawai' => $profilPegawai->id,
-            'total_potongan' => 5000.00,
-            'alasan_potongan' => 'Tidak Achievement',
-            'bukti_potongan' => 'tes.jpg',
+        $response = $this->postJson("api/potongan-karyawan/potongan-karyawan/{$potongan->id}/update_status", [
             'status' => 'DIBATALKAN',
-            'total_diambil' => '100.00',
         ]);
 
         $response->assertStatus(200);
         $this->assertDatabaseHas('potongan_karyawans', [
             'id' => $potongan->id,
             'status' => 'DIBATALKAN',
+        ]);
+    }
+    public function testUpdatePotonganKaryawanDIAJUKAN()
+    {
+        $user = UserFactory::new()->create();
+        $team = Team::create([
+            "nama" => "TestTeam",
+            "deskripsi" => "TestTeam",
+            'perusahaan_id' => 1,
+            'cabang_id' => 1,
+            "quota_kasbon_bulanan" => 10000,
+        ]);
+        $profilPegawai = ProfilPegawai::create([
+            'user_id' => $user->id,
+            'kode_pegawai' => "PGW" . date('YmdHis') . rand(100, 999),
+            'perusahaan_id' => 1,
+            'cabang_id' => 1,
+            'alamat' => 'Jl. Sudirman No. 123',
+            'jenis_kelamin' => 'Laki-laki',
+            'agama' => 'Islam',
+            'tanggal_lahir' => '1990-01-01',
+            'tanggal_masuk' => '2021-01-01',
+            'tanggal_keluar' => null,
+            'status_kawin' => true,
+            'nomor_ktp' => '1234567890',
+            'npwp' => '1234567890',
+            'gaji_pokok' => 5000000,
+            'uang_hadir' => 500000,
+            'tunjangan_jabatan' => 1000000,
+            'tunjangan_tambahan' => 500000,
+            'extra_rajin' => 500000,
+            'thr' => 1000000,
+            'tunjangan_lembur' => 500000,
+            'quota_cuti_tahunan' => 12,
+            'team_id' => $team->id,
+        ]);
+        $this->actingAs($user);
+
+        $potongan = PotonganKaryawan::create([
+            'bulan' => 11,
+            'tahun' => 2023,
+            'id_profile_pegawai' => $profilPegawai->id,
+            'total_potongan' => 5000.00,
+            'alasan_potongan' => 'Tidak Achievement',
+            'bukti_potongan' => 'tes.jpg',
+            'status' => 'BARU',
+            'total_diambil' => '100.00',
+            'perusahaan_id' => '1',
+            'cabang_id' => '1'
+        ]);
+
+        $response = $this->postJson("api/potongan-karyawan/potongan-karyawan/{$potongan->id}/update_status", [
+            'status' => 'DIAJUKAN',
+        ]);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('potongan_karyawans', [
+            'id' => $potongan->id,
+            'status' => 'DIAJUKAN',
         ]);
     }
 }
